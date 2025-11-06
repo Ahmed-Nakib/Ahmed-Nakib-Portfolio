85f4f614508749bcb3da1b87dd8d41c649f3a593
Ahmed-Nakib <office.nakib@gmail.com>
2025-11-07 01:14:47 +0600

    commit- six

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
-import Home from './page/Home'
-import Hero from './components/Hero'
 import Header from './components/Header'
+import { Toaster } from 'react-hot-toast'
+import Hero from './components/Hero'
+import Skills from './components/Skills'
+import Service from './components/Service'
+import UserComment from './components/UserComment'
+import Portfolio from './components/Portfolio'
 
 const App = () => {
   return (
     <div>
+      <Toaster />
       <Header />
       <Hero />
-      <Home />
+      <Skills />
+      <Service />
+      <UserComment />
+      <Portfolio />
     </div>
   )
 }
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-import About from '../components/About'
+import Skills from './components/Skills'
+import Service from './components/Service'
+import UserComment from './components/UserComment'
+import Portfolio from './components/Portfolio'
+import Contact from './components/Contact'
+import Hero from '../components/Hero'
 
 const Home = () => {
   return (
     <div>
-      <About />
+      <Hero />
+      <Skills />
+      <Service />
+      <UserComment />
+      <Portfolio />
+      <Contact />
     </div>
   )
 }
